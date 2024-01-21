8aa5e0999df2bfa1a1858afef16b3816fcda54e4
lorenzoAgainAgainAgain <lorenzo.utils@gmail.com>
2024-01-21 20:32:00 +0100

    NEW - ITO Project Set up

---
+++ ItoBackEnd/src/main/java/com/ito/admin/ItoBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.ito.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItoBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItoBackEndApplication.class, args);
+	}
+
+}
